af19e469a582a50c8682bdbfdbe05921021a79a0
Ricardo Bernal <lumafreco@users.noreply.github.com>
2017-09-26 19:19:16 -0500

    TallerGoogleMaps

--- TallerMapas/app/src/main/java/com/javeriana/ricardo/tallermapas/MapsActivity.java
+++ TallerMapas/app/src/main/java/com/javeriana/ricardo/tallermapas/MapsActivity.java
@@ -39,8 +39,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng bogota = new LatLng(4.65, -74.05);
+        mMap.addMarker(new MarkerOptions().position(bogota).title("Marker in Bogota"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bogota));
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+
     }
 }
